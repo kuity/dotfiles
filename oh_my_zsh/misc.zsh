b9ee46cf7b6035af78b5905a82392f7b9d62c92a
Jiang Lingzhang <lingzhang.jiang@shopee.com>
2020-10-19 08:52:30 +0800

    check fzf path before source

--- oh_my_zsh/misc.zsh
+++ oh_my_zsh/misc.zsh
@@ -1,2 +1,7 @@
-source /usr/share/fzf/key-bindings.zsh
+if [ -f /usr/share/fzf/key-bindings.zsh ]; then
+    source /usr/share/fzf/key-bindings.zsh
+fi
+
+if [ -f /usr/share/fzf/completion.zsh ]; then
 source /usr/share/fzf/completion.zsh
+fi
